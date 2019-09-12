cc49d572f96c04d51696e3d4ccf7f63f4c3231eb
Jonathan Steven <yondercode@gmail.com>
2019-09-12 16:37:45 +0700

    Applied library structure

--- src/index.ts
+++ src/index.ts
@@ -13,12 +13,49 @@ export type DatabaseDriver = 'mysql' | 'postgresql' | 'sql-server' | 'oracle'
 /**
  * Exports all functions
  */
-import mySqlConnectionParser from './drivers/mysql/parsers/connection'
+import mySqlConnectionParser from './sql/drivers/mysql/parsers/connection'
+import * as columnModels from './sql/model/column'
+import * as databaseModels from './sql/model/database'
+import * as tableModels from './sql/model/table'
+import * as referenceModels from './sql/model/references'
 
 export default {
-  mysql: {
+  sql: {
+    models: {
+      ...columnModels,
+      ...databaseModels,
+      ...tableModels,
+      ...referenceModels,
+    },
     parsers: {
-      connection: mySqlConnectionParser,
+      mysql: {
+        connection: mySqlConnectionParser,
+        query: undefined,
+      },
+      postgresql: {
+        connection: undefined,
+        query: undefined,
+      },
     },
   },
+  jsonSchema: {
+    interfaces: undefined,
+    generators: {
+      sql: undefined, // JsonSchemaSqlGenerator - Generate JSON Schema from SQL model
+    },
+    transformers: undefined,
+  },
+  typescript: {
+    generator: undefined, // TypeScriptJsonSchemaGenerator - Generate TypeScript definitions from JSON Schema
+    parser: undefined, // TypeScriptParser - Parses typescript into JSON schema
+  },
+  graphql: {
+    generator: undefined, // GraphqlGenerator - Generate GraphQL types from JSON schema,
+    parser: undefined, // GraphqlParser - Generate GraphQL types to JSON schema
+  },
+  protobuf: {
+    generator: undefined, // ProtoGenerator - Generate GraphQL types from JSON schema,
+    parser: undefined, // ProtoParser - Generate GraphQL types to JSON schema
+  },
+  filesGroup: undefined,
 }
